4f80b19426b3317c7c704f27bf609fc4820d5531
Aarne Harju <aarne.harju@sogeti.com>
2024-01-04 15:31:16 +0200

    click on element done

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,12 +1,16 @@
 import {test, expect} from '@playwright/test'
 
 test('Simple basic test', async ({page}) => {
-  await page.goto('https://www.example.com');
+  await page.goto('https://www.example.com')
   const pageTitle = await page.locator('h1')
   await expect(pageTitle).toContainText('Example Domain')
 })
 
 test("Clicking on Elements", async ({page}) => {
   await page.goto("http://zero.webappsecurity.com/index.html")
-  // await page.click()
+  await page.click("#signin_button")
+  await page.click("text=Sign in")
+
+  const errorMessage = await page.locator(".alert_error")
+  await expect(errorMessage).toContainText("Login and/or password are wrong.")
 })
