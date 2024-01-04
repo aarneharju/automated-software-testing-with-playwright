55d0c0aacfcb6e0336c24f944aff6e75350e45d7
Aarne Harju <aarne.harju@sogeti.com>
2024-01-04 16:14:23 +0200

    selectors done

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -14,3 +14,24 @@ test("Clicking on Elements", async ({page}) => {
   const errorMessage = await page.locator(".alert_error")
   await expect(errorMessage).toContainText("Login and/or password are wrong.")
 })
+
+test("Selectors", async ({page}) => {
+  /* 
+  // text
+  await page.click("text=some text")
+
+  // css selectors
+  await page.click("button")
+  await page.click("#id-name")
+  await page.click(".class-name")
+
+  // only visible css selector
+  await page.click(".submit-button:visible")
+
+  // combinations
+  await page.click(".username .first") // playwright will click an element that has class username and class of first
+
+  // xpath
+  await page.click("//button")
+ */
+})
