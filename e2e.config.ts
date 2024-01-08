a2b81534521f176744f59abd8d25662288442e28
Aarne Harju <aarne.harju@sogeti.com>
2024-01-08 14:09:39 +0200

    e2e project start

---
+++ e2e.config.ts
@@ -0,0 +1,34 @@
+import { PlaywrightTestConfig } from "@playwright/test";
+
+const config: PlaywrightTestConfig = {
+    timeout: 5000,
+    retries: 0,
+    testDir: "tests/e2e"
+    use: {
+        headless: false,
+        viewport: {
+            width: 1280,
+            height: 720
+        },
+        actionTimeout: 5000,
+        ignoreHTTPSErrors: true,
+        video: "retain-on-failure",
+        screenshot: "only-on-failure",
+    },
+    projects: [
+        {
+            name: "Chromium",
+            use: {browserName: "chromium"},
+        },
+        {
+            name: "Firefox",
+            use: {browserName: "firefox"},
+        },
+        {
+            name: "Webkit",
+            use: {browserName: "webkit"}
+        }
+    ]
+}
+
+export default config
