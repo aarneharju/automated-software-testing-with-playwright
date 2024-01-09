b6a8a0ae61315b4369e689e26e961c6ab11d0c7d
Aarne Harju <aarne.harju@sogeti.com>
2024-01-09 16:25:33 +0200

    create page model for home page done

--- tests/e2e/e2e-login.spec.ts
+++ tests/e2e/e2e-login.spec.ts
@@ -1,37 +1,31 @@
 import { test, expect } from "@playwright/test"
 import { LoginPage } from "../../page-objects/LoginPage"
+import { HomePage } from "../../page-objects/HomePage"
 
 test.describe.parallel("Login / logout flow", () => {
     let loginPage: LoginPage
+    let homePage: HomePage
+
     // Before hook
     test.beforeEach(async ({ page }) => {
+        homePage = new HomePage(page)
         loginPage = new LoginPage(page)
-        await loginPage.visit()
-        //await page.goto("http://zero.webappsecurity.com/")
+        await homePage.visit()
     })
-
+    
     // Negative scenario
     test("Negative scenario for login", async ({ page }) => {
-        await page.click("#signin_button")
+        await homePage.clickOnSignIn()
         await loginPage.login("invalid username", "invalid password")
         await loginPage.assertErrorMessage()
-        
-        // await page.locator("#user_login").fill("invalid username")
-        // await page.locator("#user_password").fill("invalid password")
-        // await page.click("text=Sign in")
-
-        // const errorMessage = await page.locator(".alert-error")
-        // await expect(errorMessage).toContainText("Login and/or password are wrong.")
     })
 
     // Positive scenario + logout
     test("Positive scenario for login + logout", async ({ page }) => {
-        await page.click("#signin_button")
-        await page.locator("#user_login").fill("username")
-        await page.locator("#user_password").fill("password")
-        await page.click("text=Sign in")
+        await homePage.clickOnSignIn()
+        await loginPage.login("username", "password")
 
-        // SSL certificate error fix (chrome or firefox will throw error because of missing sertificate, edge doesn't. If you click back, you can access the site and are logged in however.)
+        // SSL certificate error fix (chrome or firefox will throw error because of missing sertificate, edge=webkit doesn't. If you click back, you can access the site and are logged in however.)
         await page.goto("http://zero.webappsecurity.com/bank/account-summary.html")
 
         const elementToConfirm = await page.locator("#account_summary_tab")
