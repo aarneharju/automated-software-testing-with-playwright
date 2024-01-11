05ffa01584dd7dd81bb67663ae1a266bc29147e4
Aarne Harju <aarne.harju@sogeti.com>
2024-01-11 16:18:18 +0200

    post request test done

--- tests/api/api.spec.ts
+++ tests/api/api.spec.ts
@@ -24,7 +24,54 @@ test.describe.parallel("API testing", () => {
         expect(responseBody.data.id).toBe(3)
         expect(responseBody.data.first_name).toContain("Emma")
         expect(responseBody.data.email).toBeTruthy()
-        console.log(await response.json());
+        // console.log(await response.json());
         
+    })
+
+    test("POST request - Create new user",  async ({ request }) => {
+        const response = await request.post(`${baseURL}/users`, {
+            data: {
+                first_name: "Pekka",
+                last_name: "Puupää"
+            }
+        })
+
+        const responseBody = await response.json()
+        // console.log(responseBody);
+
+        expect(response.ok()).toBeTruthy()
+        expect(responseBody.first_name).toContain("Pekka")
+        expect(responseBody.id).toBeTruthy()
+        expect(responseBody.createdAt).toBeTruthy()
+    })
+
+    test("POST request - Login successful", async ({ request }) => {
+        const response = await request.post(`${baseURL}/login`, {
+            data: {
+                "email": "eve.holt@reqres.in",
+                "password": "cityslicka"
+            }
+        })
+
+        // console.log(await response.json());
+
+        const responseBody = await response.json()
+
+        expect(response.ok()).toBeTruthy()
+        expect(responseBody.token).toBeTruthy()
+    })
+
+    test("POST request - Login failed",async ({ request }) => {
+        const response = await request.post(`${baseURL}/login`,{
+            data: {
+                "email": "jabba@dabba.doo",
+                "password": "dabba"
+            }
+        })
+
+        // console.log(await response.json());
+        
+        const responseBody = await response.json()
+        expect(responseBody.token).not.toBeTruthy()
     })
 })
