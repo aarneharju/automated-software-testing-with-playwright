9f89d143cde2a5a88f1d5471b889952c2c8c95a0
Aarne Harju <aarne.harju@sogeti.com>
2024-01-11 14:25:35 +0200

    get request test done

--- tests/api/api.spec.ts
+++ tests/api/api.spec.ts
@@ -8,11 +8,23 @@ test.describe.parallel("API testing", () => {
         expect(response.status()).toBe(200)
 
         const responseBody = await response.json()
-        console.log(responseBody);
+        // console.log(responseBody);
     })
 
     test("Simple API test - Assert invalid endpoint",async ({ request }) => {
         const response = await request.get(`${baseURL}/users/invalid-endpoint`)
         expect(response.status()).toBe(404)
+    })
+
+    test("GET request - Get user detail",async ({ request }) => {
+        const response = await request.get(`${baseURL}/users/3`)
+        const responseBody = await response.json()
+
+        expect(response.ok()).toBeTruthy()
+        expect(responseBody.data.id).toBe(3)
+        expect(responseBody.data.first_name).toContain("Emma")
+        expect(responseBody.data.email).toBeTruthy()
+        console.log(await response.json());
+        
     })
 })
