1fb880a43643d41d2c6846051a0a727e1e37df5a
Aarne Harju <aarne.harju@sogeti.com>
2024-01-12 11:43:42 +0200

    parametrized tests done

--- tests/tips/tips.spec.ts
+++ tests/tips/tips.spec.ts
@@ -1,6 +1,6 @@
 import { test } from '@playwright/test'
 
-test.describe("Tips & tricks section", () => {
+test.describe.parallel("Tips & tricks section", () => {
     test("TestInfo object",async ({ page }, testInfo) => {
         await page.goto("https://www.example.com")
         console.log(`This is the config file that was used and it was printed from the testInfo object: ${testInfo.config.configFile}`)
@@ -15,4 +15,14 @@ test.describe("Tips & tricks section", () => {
         test.fixme(browserName === "chromium", "Test is not stable, needs revision")
         await page.goto("https://www.example.com")
     })
+
+    // Parametrized tests
+    const people = ["Kim", "Sonja", "Raja"]
+    for (const name of people) {
+        test(`Running test for ${name}`,async ({ page }) => {
+            await page.goto("http://zero.webappsecurity.com/")
+            await page.locator("#searchTerm").fill(name)
+            await page.waitForTimeout(3000)
+        })
+    }
 })
