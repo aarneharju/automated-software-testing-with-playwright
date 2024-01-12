afaff56fda4883630dd7e5ba735644c0df1ba90b
Aarne Harju <aarne.harju@sogeti.com>
2024-01-12 11:01:35 +0200

    fixme annotation done

--- tests/tips/tips.spec.ts
+++ tests/tips/tips.spec.ts
@@ -6,8 +6,13 @@ test.describe("Tips & tricks section", () => {
         console.log(`This is the config file that was used and it was printed from the testInfo object: ${testInfo.config.configFile}`)
     })
 
-    test("Test Skip Browser",async ({ page, browserName}) => {
+    test("Test skip browser",async ({ page, browserName}) => {
         test.skip(browserName === "chromium", "Feature not ready in Chrome browser")
+        await page.goto("https://www.example.com")
+    })
+
+    test("Test fixme annotation",async ({ page, browserName}) => {
+        test.fixme(browserName === "chromium", "Test is not stable, needs revision")
         await page.goto("https://www.example.com")
     })
 })
