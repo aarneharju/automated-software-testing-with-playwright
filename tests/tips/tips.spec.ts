12e74f5056f1a5741d4c451fd60646b3a04f0939
Aarne Harju <aarne.harju@sogeti.com>
2024-01-12 10:58:12 +0200

    skip browser annotation done

--- tests/tips/tips.spec.ts
+++ tests/tips/tips.spec.ts
@@ -3,6 +3,11 @@ import { test } from '@playwright/test'
 test.describe("Tips & tricks section", () => {
     test("TestInfo object",async ({ page }, testInfo) => {
         await page.goto("https://www.example.com")
-        console.log(testInfo)
+        console.log(`This is the config file that was used and it was printed from the testInfo object: ${testInfo.config.configFile}`)
+    })
+
+    test("Test Skip Browser",async ({ page, browserName}) => {
+        test.skip(browserName === "chromium", "Feature not ready in Chrome browser")
+        await page.goto("https://www.example.com")
     })
 })
